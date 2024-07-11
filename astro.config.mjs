e57df0bbf5e7da464206c770e200490a0bd065bc
zihao <positivevector@gmail.com>
2024-07-11 19:12:48 +0800

    vercel analytic

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(), tailwind(), partytown()]
+  integrations: [mdx(), react(), tailwind(), partytown()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
