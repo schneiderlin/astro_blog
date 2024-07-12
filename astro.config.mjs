57de9d522a0a5cf7ad7139bf35bbdb92412a9b46
zihao <positivevector@gmail.com>
2024-07-12 10:10:29 +0800

    sitemap head

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://linzihao.com",
   integrations: [mdx(), react(), tailwind(), partytown(), sitemap()],
   output: "server",
   adapter: vercel({
