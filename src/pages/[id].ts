ce33f633cf53496988d3a8f1b98a68b018925352
zihao <positivevector@gmail.com>
2024-07-17 11:41:44 +0800

    current time

--- src/pages/[id].ts
+++ src/pages/[id].ts
@@ -2,7 +2,7 @@ import type { APIRoute } from "astro"
 
 export const GET: APIRoute = ({ params, request }) => {
   return new Response(JSON.stringify({
-      message: "This was a GET!"
+      message: `current time is ${Date.now()}`,
     })
   )
 }
