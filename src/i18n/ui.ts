2d8eedaea02c3146c03c513206bdbcd3bb8a376b
zihao <positivevector@gmail.com>
2024-09-09 10:01:16 +0800

    i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+    en: 'English',
+    zh: '简体中文',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        'nav.home': 'Home',
+        'nav.about': 'About',
+        'album': 'Album',
+        'articles': 'All Articles',
+        'filter.tags': 'Filter Tags',
+    },
+    zh: {
+        'nav.home': '主页',
+        'nav.about': '关于',
+        'album': '相册集',
+        'articles': '所有文章',
+        'filter.tags': '筛选标签',
+    },
+} as const;
