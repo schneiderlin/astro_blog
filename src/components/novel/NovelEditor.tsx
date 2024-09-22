4ab93533b4a652b1d6e2842c4af9b93b53650cd9
zihao <positivevector@gmail.com>
2024-09-22 13:20:50 +0800

    novel

---
+++ src/components/novel/NovelEditor.tsx
@@ -0,0 +1,54 @@
+import { EditorCommand, EditorCommandEmpty, EditorCommandItem, EditorCommandList, EditorContent, EditorRoot } from "novel";
+import { useState } from "react";
+import defaultExtensions from "../novel/extensions";
+import { slashCommand, suggestionItems } from "./slash";
+import { handleCommandNavigation } from "novel/extensions";
+
+const extensions = [...defaultExtensions, slashCommand];
+
+const TailwindEditor = () => {
+  const [content, setContent] = useState(null);
+
+  return (
+    <EditorRoot>
+      <EditorContent
+        initialContent={content}
+        extensions={extensions}
+        onUpdate={({ editor }) => {
+          const json = editor.getJSON();
+          setContent(json);
+        }}
+        editorProps={{
+          handleDOMEvents: {
+            keydown: (_view, event) => handleCommandNavigation(event),
+          },
+          attributes: {
+            class: `prose prose-lg dark:prose-invert prose-headings:font-title font-default focus:outline-none max-w-full`,
+          }
+      }}
+      >
+        <EditorCommand className='z-50 h-auto max-h-[330px]  w-72 overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all'>
+          <EditorCommandEmpty className='px-2 text-muted-foreground'>No results</EditorCommandEmpty>
+          <EditorCommandList>
+            {suggestionItems.map((item) => (
+              <EditorCommandItem
+                value={item.title}
+                onCommand={(val) => item.command(val)}
+                className={`flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:bg-accent aria-selected:bg-accent `}
+                key={item.title}>
+                <div className='flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background'>
+                  {item.icon}
+                </div>
+                <div>
+                  <p className='font-medium'>{item.title}</p>
+                  <p className='text-xs text-muted-foreground'>{item.description}</p>
+                </div>
+              </EditorCommandItem>
+            ))}
+          </EditorCommandList>
+        </EditorCommand>
+      </EditorContent>
+    </EditorRoot>
+  );
+};
+export default TailwindEditor;
